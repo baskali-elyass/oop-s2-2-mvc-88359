653ffbc6bedf4581b7a2ffc9abf310aed4e2a9b1
baskali-elyass <elyas.baskali@epita.fr>
2026-03-23 22:26:45 +0000

    Ajoutez des fichiers projet.

---
+++ oop-s2-2-mvc-88359/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace oop_s2_2_mvc_88359.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
